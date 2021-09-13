00f65130f55d688d2d2e9a64a9a1fa90d3bef23b
DanilovG103 <danilov2706@gmail.com>
2021-09-13 19:55:09 +0400

    feat: add icons

--- pokedex/components/Tabs.tsx
+++ pokedex/components/Tabs.tsx
@@ -4,13 +4,30 @@ import { Main } from '../screens/Main';
 import { Legendaries } from '../screens/Legendaries';
 import { About } from '../screens/About';
 import { Compare } from '../screens/Compare';
+import Icon from 'react-native-vector-icons/Ionicons';
 const Tab = createBottomTabNavigator();
 
 export const Tabs = () => {
   return (
     <Tab.Navigator
-      screenOptions={() => ({
-        tabBarActiveTintColor: 'tomato',
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Legendaries') {
+            iconName = focused ? 'medal' : 'medal-outline';
+          } else if (route.name === 'Compare') {
+            iconName = focused ? 'bar-chart' : 'bar-chart-outline';
+          } else {
+            iconName = focused
+              ? 'information-circle'
+              : 'information-circle-outline';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#D93E30',
         tabBarInactiveTintColor: 'gray',
         headerShown: false,
       })}>
