62d50e8db3ff01e1e1fff538dbef5256fed9ebd5
DanilovG103 <danilov2706@gmail.com>
2021-09-20 15:11:13 +0400

    fix: styles

--- pokedex/components/Navigation.tsx
+++ pokedex/components/Navigation.tsx
@@ -4,6 +4,7 @@ import { Legendaries } from '../screens/Legendaries';
 import { Compare } from '../screens/Compare';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Header } from './Header';
+import { Colors } from '../assets/colors';
 
 const Drawer = createDrawerNavigator();
 
@@ -13,8 +14,16 @@ export const Navs = () => {
       screenOptions={{
         drawerPosition: 'right',
         drawerStyle: {
+          backgroundColor: Colors.yellow1,
+          borderBottomLeftRadius: 10,
+          borderBottomRightRadius: 10,
           width: '100%',
-          height: 200,
+          height: 300,
+        },
+        drawerActiveBackgroundColor: 'transparent',
+        drawerLabelStyle: {
+          fontSize: 24,
+          color: Colors.black,
         },
         header: ({ navigation }) => {
           return <Header open={navigation.openDrawer} />;
