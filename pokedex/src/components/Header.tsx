b0a33df9a84bc0bff1675b6abdfa3d755bbb00fe
DanilovG103 <danilov2706@gmail.com>
2021-09-23 14:16:05 +0400

    fix: header menu color

--- pokedex/src/components/Header.tsx
+++ pokedex/src/components/Header.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { View, TouchableOpacity } from 'react-native';
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 import { Logo } from '../../resources/assets/images/icons/Logo';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { ModalNavigation } from './ModalNavigation';
+import { Colors } from '../theme/colors';
 
 const Container = styled(View)`
   padding: 10px 25px;
@@ -15,11 +16,17 @@ const Container = styled(View)`
 
 export const Header = ({ navigation }) => {
   const [visible, setVisible] = useState(false);
+  const theme = useTheme();
+
   return (
     <Container>
       <Logo />
       <TouchableOpacity onPress={() => setVisible(true)}>
-        <Icon name="menu" size={35} />
+        <Icon
+          name="menu"
+          size={35}
+          color={theme.type === 'dark' ? Colors.yellow1 : Colors.black}
+        />
       </TouchableOpacity>
       <ModalNavigation
         visible={visible}
