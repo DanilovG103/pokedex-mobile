943e6bc7fdc899a7a1fa75d3ba40402308f429c4
DanilovG103 <danilov2706@gmail.com>
2021-09-24 14:29:36 +0400

    fix: filter

--- pokedex/src/store/reducer/FilterReducer.tsx
+++ pokedex/src/store/reducer/FilterReducer.tsx
@@ -1,8 +1,8 @@
 const initialState = {
   experienceFrom: 0,
-  experienceTo: 100000,
+  experienceTo: 609,
   attackFrom: 0,
-  attackTo: 100000,
+  attackTo: 600,
   type: null,
   loading: false,
 };
@@ -12,29 +12,33 @@ export default function FilterReducer(state = initialState, action) {
     case 'SET_EXPERIENCE':
       return {
         ...state,
-        experienceFrom: action.payload.from,
+        experienceFrom:
+          action.payload.from > action.payload.to ||
+          action.payload.from > state.experienceTo
+            ? 0
+            : action.payload.from,
         experienceTo:
-          action.payload.to < action.payload.from + 1
-            ? 100000
-            : action.payload.to,
+          action.payload.to < action.payload.from + 1 ? 609 : action.payload.to,
       };
     case 'SET_ATTACK':
       return {
         ...state,
-        attackFrom: action.payload.from,
+        attackFrom:
+          action.payload.from > action.payload.to ||
+          action.payload.from > state.attackTo
+            ? 0
+            : action.payload.from,
         attackTo:
-          action.payload.to < action.payload.from + 1
-            ? 100000
-            : action.payload.to,
+          action.payload.to < action.payload.from + 1 ? 600 : action.payload.to,
       };
     case 'REFRESH':
       return {
         ...state,
         type: null,
         experienceFrom: 0,
-        experienceTo: 100000,
+        experienceTo: 609,
         attackFrom: 0,
-        attackTo: 100000,
+        attackTo: 600,
       };
     case 'GET_POKEMON_BY_TYPE_LOADING':
       return {
